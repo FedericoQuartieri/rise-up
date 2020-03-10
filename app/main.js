7c3503eb3e55e064b6541cc9f018591710c7eb63
FEDERICO QUARTIERI <FEDERICOQUARTIERI@Air-di-FEDERICO.homenet.telecomitalia.it>
2020-03-10 11:58:09 +0100

    prove su require.js

---
+++ app/main.js
@@ -0,0 +1,13 @@
+define(function (require) {
+    // Load any app-specific modules
+    // with a relative require call,
+    // like:
+    var attributes = require('./attributes');
+
+    // Load library/vendor modules using
+    // full IDs, like:
+});
+stati = JSON.parse(attributes)
+state_choose = (stato) => {
+  return stati[stato]
+}
