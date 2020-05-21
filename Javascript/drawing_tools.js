d0b1685df979c5e38c058fb20b59e4e623ca40e0
Lorenzo Biagi <you@example.com>
2020-05-22 00:18:37 +0200

    merge

--- Javascript/drawing_tools.js
+++ Javascript/drawing_tools.js
@@ -1169,7 +1169,6 @@ const drawing_tools={
             })
         }
 
-<<<<<<< HEAD
     },
     "display_title":{
         draw:(container_out)=>{
@@ -1248,8 +1247,6 @@ const drawing_tools={
             backdrop: 'static',
             keyboard: false
         })
-=======
->>>>>>> d56626d8d2fcc7a0fc64f7e55864456543947ed9
     }
 }
 
